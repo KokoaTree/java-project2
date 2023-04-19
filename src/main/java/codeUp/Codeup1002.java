88e0d0aac799bdfd9ee3532d8a17df9f8464b375
KokoaTree <4kakao1@daum.net>
2023-04-19 15:54:47 +0900

    feat: java Codeup1002~Codeup1003 - Hello World 출력 - Hello   World 출력 - 'Hello' 출력 - "Hello World" 출력

---
+++ src/main/java/codeUp/Codeup1002.java
@@ -0,0 +1,7 @@
+package codeUp;
+
+public class Codeup1002 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/codeUp/Codeup1005.java
@@ -0,0 +1,8 @@
+package codeUp;
+
+public class Codeup1005 {
+    public static void main(String[] args) {
+        System.out.println("\"Hello World\"");
+    }
+
+}
