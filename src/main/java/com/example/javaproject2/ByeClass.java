33f85f394306b40be02169d68699391fe9b6b974
KokoaTree <4kakao1@daum.net>
2023-04-19 11:45:12 +0900

    feat: Bye기능 추가 - ByeClass, ByeClassTest 추가

---
+++ src/main/java/com/example/javaproject2/ByeClass.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2;
+
+public class ByeClass {
+    public void print()
+        {
+            System.out.println("Bye");
+        }
+}
---
+++ src/main/java/com/example/javaproject2/ByeClassTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2;
+
+public class ByeClassTest {
+    public static void main(String[] args) {
+        ByeClass printBye = new ByeClass();
+        printBye.print();
+    }
+}
