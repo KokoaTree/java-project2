f80ba17361ae46ba613fd2a85df6fb7e360a7fc5
KokoaTree <4kakao1@daum.net>
2023-04-19 13:10:13 +0900

    feat: 마지막 크롤 시간 작업1 - CrawlDate 도메인 추가

---
+++ src/main/java/com/example/javaproject2/CrawlDate.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2;
+
+public class CrawlDate {
+    public class CrawlDate {
+        private LocalDateTime lastCrawlDateTime;
+        // alt + insert
+        public CrawlDate(LocalDateTime lastCrawlDateTime) {
+            this.lastCrawlDateTime = lastCrawlDateTime;
+        }
+    }
+}
